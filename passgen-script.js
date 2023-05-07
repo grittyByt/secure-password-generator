8d6d714e16d65f3e5d04794d8ad918110da2afe2
grittyByt <terrell_whiting@yahoo.com>
2023-05-07 17:18:11 -0400

    added another function/ added some alets and confirm prompts/working to generate the random password

--- passgen-script.js
+++ passgen-script.js
@@ -29,47 +29,105 @@ let numChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 let specChar = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '?', '<', '>', '/', ',', '.'];
 // let numCheck = prompt('How many characters do you want in your password?', 'Enter 8 - 128');
 
-function generatePassword(){
-  let lengthCheck = prompt('How many characters do you want in your password?', 'Enter 8 - 128');
-  lengthCheck;
-  if(lengthCheck >= 8 && lengthCheck < 129 && lengthCheck){
-    let allCap = confirm('Do you want to have capitalized letters in your password?', 'Press OK');
-    allCap;
-  } else if(lengthCheck === null){
-    alert('Cannot be left blank!');
-    return generatePassword();
-  } else {
-    alert('A number between 8 and 128 must be selected to continue.')
-    return generatePassword();
-  };
-  
-  if(allCap){
-    let allLow = confirm('Do you want to have lowercase letters in your password?', 'Press OK');
-    allLow;
-  } else if(allCap === null){
-    alert('Cannot be left blank!');
-    return allCap;
-  } else {
-    alert('It"s highly suggested to have a CAPTILIZED letter to better secure the password.');
-    return generatePassword();
-  };
+function generateChoices(){
+   // adding parseInt method around the prompt will prevent weirdness from happening if user enters a number with decimal (non integer)
+   let lengthCheck = parseInt(prompt('How many characters do you want in your password?', 'Enter 8 - 128'));
+   lengthCheck;
+   if(lengthCheck >= 8 && lengthCheck < 129){
+     let allCap = confirm('Do you want to have capitalized letters in your password?', 'Press OK');
+     allCap;
+   } else if(lengthCheck === null){
+     alert('Cannot be left blank!');
+     return generatePassword();
+   } else {
+     alert('A number between 8 and 128 must be selected to continue.')
+     return generatePassword();
+   };
+   
+   // after selecting OK the user will be prompted several more times with these if statements.
+   if(allCap === true){
+     let allLow = confirm('Do you want to have lowercase letters in your password?', 'Press OK');
+     allLow;
+   } else if(allCap === null){
+     alert('Cannot be left blank!');
+     return allCap;
+   } else {
+     alert('It"s highly suggested to have a CAPTILIZED letter to better secure the password.');
+     return generatePassword();
+   };
+ 
+   if(allLow){
+     let specCheck = confirm('Do you want to have special characters in your password?', 'Press OK');
+     specCheck;
+   } else if(allLow === null){
+     alert('Cannot be left blank!');
+     return allLow;
+   } else {
+     alert('It"s highly suggested to have a lowercase letter to better secure the password.');
+     return generatePassword();
+   };
+ 
+   if(specCheck){
+     let numCheck = confirm('Do you want to have numbers in your password?', 'Press OK');
+     numCheck;
+   } else if (numCheck === null){
+     alert('Cannot be left blank?');
+     return numCheck;
+   } else {
+     alert('It"s highly suggested to have a lowercase letter to better secure the password.');
+     return generatePassword();
+   }
 
-  if(allLow){
-    let specCheck = confirm('Do you want to have special characters in your password?', 'Press OK');
-    specCheck;
-  } else if(allLow === null){
-    alert('Cannot be left blank!');
-    return allLow;
-  } else {
-    alert('It"s highly suggested to have a lowercase letter to better secure the password.');
-    return generatePassword();
-  };
+  //  object containing the confirmation decison by the user
+   let passChoices = {
+    passLength: lengthCheck,
+    passUp: allCap,
+    passLow: allLow,
+    passNum: numCheck,
+    passSpec: specCheck
+   }
 
-  if(specCheck){
-    let numCheck = prompt('DO you want to have numbers in your password?', 'Press OK');
-    numCheck;
-    
+   if(numCheck){
+    return passChoices;
+   } else if (numCheck === null){
+    alert('Cannot be left blank?');
+    return numCheck;
+  } else {
+    alert('It"s highly suggested to have a number to better secure the password.');
+    return generatePassword();
   }
+
+}
+
+function generatePassword(){
+  // when the button is pressed the user is prompted with their first question
+ let userConfirmation = generateChoices();
+ let userConfirmed = '';
+ let pass = '';
+
+ if(userConfirmation.passUp){
+  userConfirmed += upCap.join('');
+ }
+
+ if(userConfirmation.passLow){
+  userConfirmed += lowLett.join('');
+ }
+
+ if(userConfirmation.passSpec){
+  userConfirmed += specChar.join('');
+ }
+
+ if(userConfirmation.passNum){
+  userConfirmed += numChar.join('');
+ }
+
+ for(howLong = 0; howLong < userConfirmation.passLength; howLong++){
+  let atRandom = Math.floor(Math.random() * userConfirmed.length);
+  pass += userConfirmed.charAt(atRandom);
+ }
+
+ return pass;
+
 }
 
 
